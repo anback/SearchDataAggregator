253fb2be611a1ddc7e096eff5fe757eef86313a5
anback <anders@swedishtravelmafia.com>
2014-08-22 11:57:37 +0200

    fd

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 var mubsub = require('mubsub');
 var client = mubsub('mongodb://swalo:84kAanan@ds051658.mongolab.com:51658/swalo');
-
+var datejs = require('datejs');
 var addonChannel = client.channel('addons');
 var searchdatachannel = client.channel('searchdata');
+var searchChannel = client.channel('searches');
 
 client.on('error', console.error);
 addonChannel.on('error', console.error);
 searchdatachannel.on('error', console.error);
+searchChannel.on('error', console.error);
 
-searchdatachannel.subscribe('NewRes', function (message) {
+
+var c = 0;
+var d = 0;
+searchdatachannel.subscribe('NewSearchData', function (message) {
+    
+    d++;
+    if(!message.Itineraries.some(function(itinerary) {
+        return itinerary.prices.some(function(price) {
+            return price.OTA == 'Swalo'
+        })
+    }))
+    {
+        c++;
+        console.log("No Swalo Price: " + c/d);
+    }
+
     var res = GetData(message);
-
-    res = checkForDuplicates(res);
 
     res.forEach(function(item) {
 
-        if(item.isDuplicate) {
-            console.log("avoided duplicate insertion");
-            return;
-        }
+        addonChannel.publish("NewAddonProposal", item);
 
-        if(item.order <= 1 || (item.Markup > 5 && item.order < 5))
-            addonChannel.publish("NewAddon", item);
-        else
-            addonChannel.publish("NewAddonProposal", item);
+        if(item.Markup > 0)
+        {
+            publishNewSearches(item); 
+            addonChannel.publish("NewAddon", item)
+        }
     });
 });
+
+function publishNewSearches(item) {
+
+    for (var i =  - 3; i >= 3; i++) {
+        var outboundDate = Date.parse(item.OutboundLegDepartureDateTime).add({ days : i});
+        var inboundDate = Date.parse(item.InboundLegDepartureDateTime).add({ days : i});
+
+        var res = {
+            in_DepartureDate : outboundDate,
+            in_ReturnDate : inboundDate,
+            in_FromCity : item.Origin,
+            in_ToCity : item.Dest
+        };
+
+        searchChannel.publish('NewSearch', res);
+    };
+}
 
 function checkForDuplicates(res) {
     for (var i = res.length - 1; i >= 0; i--)
@@ -54,6 +84,16 @@ function GetData(message) {
         
         itinerary.order = index;
 
+        itinerary.prices = itinerary.prices.map(function(item) {
+
+            if(item.Price.indexOf('€') != -1)
+            {
+                item.Price = item.Price.split('€')[0].trim();
+            }
+
+            return item;
+        });
+
         competitorprices = itinerary.prices.filter(function(price) {
             return price.OTA !== 'Swalo'
         });
@@ -68,8 +108,12 @@ function GetData(message) {
         if(swaloprices[0].Price == undefined)
             return undefined;
 
+        itinerary.SwaloPrice = swaloprices[0].Price.replace('.','');
+
+
         if(competitorprices.length == 0 || competitorprices[0].Price == undefined) //We are the only one
         {
+            return undefined;
             console.log("We are the only one!");
             itinerary.Markup = -30.0;
         }
@@ -83,34 +127,20 @@ function GetData(message) {
         if(itinerary.InboundLegDepartureDateTime == undefined)
             return undefined;
         
-        
         itinerary.OutboundLegDepartureDateTime += ":00";
-        itinerary.InboundLegDepartureDateTime += ":00"; 
-        
-
+        itinerary.InboundLegDepartureDateTime += ":00";
 
         if(itinerary.Markup != -30.0)
         {
-            itinerary.SwaloPrice = swaloprices[0].Price.replace('.','');
             itinerary.BestCompetitorPrice = competitorprices[0].Price.replace('.','');
             itinerary.Markup = parseFloat(itinerary.SwaloPrice) - parseFloat(itinerary.BestCompetitorPrice);
         }
-        /*
-        console.log("SwaloPrice: ");
-        console.log(itinerary.SwaloPrice);
 
-        console.log("BestCompetitorPrice: ");
-        console.log(itinerary.BestCompetitorPrice);
-        */
-
-        logItinerary(itinerary);
-        
-        if(itinerary.Markup > 8.9)
-            return undefined;
-        
         itinerary.Markup = itinerary.Markup * -1 - 0.1;
 
         delete itinerary.prices;
+
+        logItinerary(itinerary);
         
         return itinerary;
     });
@@ -122,6 +152,12 @@ function GetData(message) {
     return res;
 }
 
-function logItinerary(itinerary)  {
-    console.log(itinerary.Origin + ' ' + itinerary.Dest + ' ' + itinerary.OutboundLegDepartureDateTime + ' ' + itinerary.InboundLegDepartureDateTime + ' ' + itinerary.CarrierIds + ' ' + itinerary.Markup + ' ' + itinerary.order );
+function logItinerary(itinerary, suffix)  {
+
+    if(suffix == undefined)
+        suffix = '';
+    console.log(itinerary.Origin + ' ' + itinerary.Dest + ' ' + ' ' + itinerary.CarrierIds + ' ' + itinerary.SwaloPrice +  ' ' + itinerary.Markup + ' ' + itinerary.order + ' ' + suffix);
 }
+
+
+
